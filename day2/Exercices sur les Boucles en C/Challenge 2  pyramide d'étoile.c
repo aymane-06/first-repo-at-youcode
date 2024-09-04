c92a207949976a3ae2d0fcc4d911e08b747269c1
aymane-06 <aymane.himame@gmail.com>
2024-09-04 16:43:30 +0200

    3rd commit

---
+++ day2/Exercices sur les Boucles en C/Challenge 2  pyramide d'étoile.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+ int main(){
+ int star;
+ printf("enter le nombre des lignes pour composer le traingle:");
+ scanf("%d",&star);
+ for(int i=1;i<=star;i++){
+
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+ return 0;
+ }
